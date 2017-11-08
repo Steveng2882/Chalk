1e12f315c231da4c7d4ad96eff66a6545a1f76ea
Steveng2882 <31765885+Steveng2882@users.noreply.github.com>
2017-11-07 23:44:04 -0600

    Delete index1.js

--- index1.js
+++
@@ -1,5 +0,0 @@
-const chalk = require('chalk');
-
-console.log(chalk.blue.bold('H') + chalk.red('E') + chalk.yellow('ll') + 
-chalk.green('O') + (' ') + chalk.magentaBright('w') + chalk.cyan('O') + chalk.gray('R') +
-chalk.blueBright('L') + chalk.redBright('D') + chalk.yellowBright('!'));

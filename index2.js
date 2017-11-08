d52578f0cd81f0cce2695e1c4e4898f41c11fd37
Steveng2882 <31765885+Steveng2882@users.noreply.github.com>
2017-11-07 23:44:29 -0600

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+
+
+    var Items =  [
+    {
+        fname: "rafael",
+        lname: "marques",
+        age: "19"},
+    {
+        fname: "daniel",
+        lname: "marques",
+        age: "19"
+    }]
+
+    _.forEach(Items, value=>{
+        console.log(value)
+    })
